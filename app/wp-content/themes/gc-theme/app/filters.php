d8921d07df3b9245979a8dbd51bc453a9800a81b
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-11 10:40:22 -0300

    advc-8 Ajusta menu mobile

--- app/wp-content/themes/gc-theme/app/filters.php
+++ app/wp-content/themes/gc-theme/app/filters.php
@@ -16,10 +16,18 @@ add_filter('excerpt_more', function () {
 });
 
 add_filter('nav_menu_css_class', function($classes, $item, $args) {
-    if(isset($args->item_classes))
+    if(property_exists($args, 'item_classes'))
         $classes[] = $args->item_classes;
     if(!empty($index = array_search('current-menu-item', $classes)))
         $classes[$index] = '!text-primary';
 
     return $classes;
 }, 1, 3);
+
+
+add_filter( 'nav_menu_link_attributes', function($atts, $item, $args) {
+    if(property_exists($args, 'link_classes'))
+      $atts['class'] = $args->link_classes;
+
+    return $atts;
+}, 1, 3 );
--- app/wp-content/themes/gc-theme/resources/views/sections/header.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/sections/header.blade.php
@@ -1,12 +1,12 @@
-<header class="bg-black/20 py-10 lg:py-[22px] px-3.5 lg:px-9 flex items-center justify-between">
+<header class="bg-black/20 h-36 md:h-[92px] px-3.5 md:px-9 flex items-center justify-between">
   <a class="z-10" href="{{ home_url('/') }}">
-    <img class="h-16 lg:h-12" src="@asset('images/logo-site.svg')" alt="{{ __('Logo General Conference') }}">
+    <img class="h-16 md:h-12" src="@asset('images/logo-site.svg')" alt="{{ __('Logo General Conference') }}">
   </a>
 
   @if(has_nav_menu('primary_navigation'))
     <input id="menu-toggler" class="hidden" type="checkbox" autocomplete="off">
 
-    <label for="menu-toggler" class="cursor-pointer flex items-center text-primary font-bold text-xs px-2 py-3 bg-[#424A5E52] rounded-lg lg:hidden z-10">
+    <label for="menu-toggler" class="cursor-pointer flex items-center text-primary font-bold text-xs px-2 py-3 bg-[#424A5E52] rounded-lg md:hidden z-10">
       <div class="mr-1.5 grid content-between h-3">
         <span class="h-0.5 rounded-sm bg-primary w-5 block"></span>
         <span class="h-0.5 rounded-sm bg-primary w-5 block"></span>
@@ -15,14 +15,18 @@
       menu
     </label>
 
-    <nav class="fixed lg:relative top-0 left-full bottom-0 pt-36 lg:pt-0 bg-secondary lg:bg-transparent w-full lg:w-auto transition-all" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+    <nav
+      class="fixed md:relative top-0 left-full md:left-0 bottom-0 pt-36 md:pt-0 bg-secondary md:bg-transparent w-full md:w-auto transition-all flex justify-center"
+      aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}"
+    >
       {!!
         wp_nav_menu([
           'theme_location' => 'primary_navigation',
-          'menu_class'     => 'flex flex-col lg:flex-row',
+          'menu_class'     => 'flex flex-col md:flex-row',
           'container'      => false,
           'echo'           => false,
-          'item_classes'   => 'ml-6 font-bold uppercase text-[#E5E5E5] hover:underline text-sm',
+          'item_classes'   => 'md:ml-6 font-bold uppercase text-[#E5E5E5] hover:underline text-sm flex justify-center',
+          'link_classes'   => 'py-3',
         ])
       !!}
     </nav>
