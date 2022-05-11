bf41ec1d1f603ae9e0b0b954c35a40a62b08bb33
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-11 16:31:17 -0300

    advc-10 Ajusta espaçamentos

--- app/wp-content/themes/gc-theme/resources/views/partials/page-header.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/partials/page-header.blade.php
@@ -1,9 +1,9 @@
 <section class="w-full relative flex justify-start flex-col items-center pt-14 pb-4 md:pt-4">
   <img class="h-full w-full absolute top-0 left-0 object-cover -z-10" src="@asset('images/page-header.jpg')" alt="{{ __('Imagem de fundo') }}">
 
-  <img class="h-14 md:h-20 mb-2" src="@asset('images/page-header-logo.png')" alt="{{ __('Uma igreja em movimento') }}">
+  <img class="h-14 md:h-20 mb-2 mt-24 md:mt-40" src="@asset('images/page-header-logo.png')" alt="{{ __('Uma igreja em movimento') }}">
 
-  <div class="live w-full px-4 max-w-container">
+  <div class="live w-full px-4 max-w-container mt-7 md:mt-20">
     <h4
       class="before:content-[''] before:w-4 before:h-4 before:bg-[#FF0000] before:flex before:rounded-full before:shadow-lg before:shadow-[#ff000066] before:mr-3 before:md:mr-2
       flex items-center text-primary font-bold text-sm md:text-base mb-4 justify-center md:justify-start"
