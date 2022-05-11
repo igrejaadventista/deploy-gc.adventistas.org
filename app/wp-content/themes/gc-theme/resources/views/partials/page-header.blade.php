39af3e78e6a8b3fe338b3883a365344324c1f6bb
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-11 13:20:49 -0300

    advc-10 Inicia page header

--- app/wp-content/themes/gc-theme/resources/views/partials/page-header.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/partials/page-header.blade.php
@@ -1,3 +1,5 @@
-<div class="page-header">
-  <h1>{!! $title !!}</h1>
-</div>
+<section class="w-full relative flex justify-center flex-row items-start pt-40 md:pt-[182px] pb-4">
+  <img class="h-full w-full absolute top-0 left-0 object-cover -z-10" src="@asset('images/page-header.jpg')" alt="{{ __('Imagem de fundo') }}">
+
+  <img class="h-14 md:h-20" src="@asset('images/page-header-logo.png')" alt="{{ __('Uma igreja em movimento') }}">
+</section>
