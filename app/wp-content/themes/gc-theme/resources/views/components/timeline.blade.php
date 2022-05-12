d45892af497330a40e7c9e94a3e9f8a82973a6f7
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-12 16:45:28 -0300

    advc-12 Inicia timeline

---
+++ app/wp-content/themes/gc-theme/resources/views/components/timeline.blade.php
@@ -0,0 +1,6 @@
+<section
+  class="timeline w-full px-4 mx-auto max-w-container relative mt-6 md:mt-16
+  before:content-[''] before:absolute before:w-1 before:top-0 before:h-full before:bg-primary"
+>
+  <h2 class="text-secondary text-2xl p-4 border-l-8 border-primary bg-gradient-to-r from-primary/20 to-primary/0">Notícias de <span class="font-bold">hoje</span></h2>
+</section>
