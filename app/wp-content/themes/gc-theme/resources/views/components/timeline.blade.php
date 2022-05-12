ebd258aa96c01bdb98f5d34b01ce44e3bd9cc79d
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-12 18:24:08 -0300

    advc-12 Cria box de texto

--- app/wp-content/themes/gc-theme/resources/views/components/timeline.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/components/timeline.blade.php
@@ -43,4 +43,28 @@
   >
     Notícias de <span class="font-bold">05/04</span>
   </h2>
+
+  <article class="mb-12 md:mb-14">
+    <div class="border-l-8 border-secondary pl-4">
+      <h4 class="text-primary font-bold text-xs">às 18:00</h4>
+
+      <h3 class="text-secondary font-bold text-xl md:text-2xl">Orientação 1</h3>
+    </div>
+
+    <div class="pl-6 flex flex-col mt-2">
+      <div class="p-4 md:p-6 bg-[#F8F9FB] rounded-lg">
+        <p class="text-sm md:text-base text-grey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nibh ipsum, facilisis ac vehicula ut, interdum eget lacus. Maecenas faucibus cursus tellus nec pellentesque. Aenean blandit est eu ultrices sagittis.
+        </p>
+
+        <p class="mt-4 text-sm md:text-base text-grey">
+          <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nibh ipsum</b>, facilisis ac vehicula ut, interdum eget lacus.
+        </p>
+
+        <a class="btn-secondary py-2 px-3 text-sm mt-4 md:mt-6" href="#">
+          Ler mais<img class="ml-2 h-3" src="@asset('images/arrow-down.svg')" alt="{{ __('Seta para baixo') }}">
+        </a>
+      </div>
+    </div>
+  </article>
 </section>
