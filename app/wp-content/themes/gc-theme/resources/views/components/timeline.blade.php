88bcf0d6100c2b708c69fda786738be0d01fde62
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-12 17:34:02 -0300

    advc-12 Cria notícia

--- app/wp-content/themes/gc-theme/resources/views/components/timeline.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/components/timeline.blade.php
@@ -2,5 +2,25 @@
   class="timeline w-full px-4 mx-auto max-w-container relative mt-6 md:mt-16
   before:content-[''] before:absolute before:w-1 before:top-0 before:h-full before:bg-primary"
 >
-  <h2 class="text-secondary text-2xl p-4 border-l-8 border-primary bg-gradient-to-r from-primary/20 to-primary/0">Notícias de <span class="font-bold">hoje</span></h2>
+  <h2
+    class="text-secondary text-2xl md:text-[32px] md:leading-10 p-4 md:py-3 border-l-8 border-primary bg-gradient-to-r from-primary/20 to-primary/0 mb-6"
+  >
+    Notícias de <span class="font-bold">hoje</span>
+  </h2>
+
+  <article class="mb-12 md:mb-14">
+    <div class="border-l-8 border-secondary pl-4">
+      <h4 class="text-primary font-bold text-xs">às 23:00</h4>
+
+      <h3 class="text-secondary font-bold text-xl md:text-2xl">Abertura do evento</h3>
+    </div>
+
+    <div class="pl-6 flex flex-col">
+      <img class="w-full rounded-lg aspect-thumbnail object-cover mt-4 md:order-2" src="@asset('images/timeline-01.png')" alt="Abertura do evento">
+
+      <p class="mt-3 md:mt-2 text-sm md:text-base text-grey">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nibh ipsum, facilisis ac vehicula ut, interdum eget lacus. Maecenas faucibus cursus tellus nec pellentesque. Aenean blandit est eu ultrices sagittis.
+      </p>
+    </div>
+  </article>
 </section>
