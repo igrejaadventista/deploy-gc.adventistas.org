aee52bfeb2a82bb7179c9c51259282b479472aa6
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-13 14:03:40 -0300

    advc-13 Adiciona imagem de destaque

--- app/wp-content/themes/gc-theme/resources/views/page.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/page.blade.php
@@ -4,6 +4,12 @@
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
 
+    @if(has_post_thumbnail())
+      <div class="aspect-thumbnail max-h-[572px] w-full">
+        <img class="w-full h-full object-cover" src="@thumbnail('full', false)" alt="@title">
+      </div>
+    @endif
+
     @includeFirst(['partials.content-page', 'partials.content'])
   @endwhile
 @endsection
