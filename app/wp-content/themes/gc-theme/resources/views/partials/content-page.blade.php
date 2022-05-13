053f2c07757d4a7a747f48ae0144464ce8872be5
Tiago Papile <tpapile@studiovisual.com.br>
2022-05-13 10:50:37 -0300

    advc-13 Cria footer

--- app/wp-content/themes/gc-theme/resources/views/partials/content-page.blade.php
+++ app/wp-content/themes/gc-theme/resources/views/partials/content-page.blade.php
@@ -1,9 +1,19 @@
-<article class="w-full px-4 mx-auto max-w-container pt-6 md:pt-9">
+<article class="w-full px-4 mx-auto max-w-container pt-6 md:pt-9 pb-14 md:pb-24">
   <header class="w-full flex flex-col pb-2 mb-6 border-b border-grey-light">
     <h1 class="text-2xl md:text-[2.5rem] md:leading-[3rem] font-bold text-secondary block">@title</h1>
 
     <time class="text-primary font-bold text-xs text-right mt-4">@published</time>
   </header>
 
-  @content
+  <section>
+    @content
+  </section>
+
+  <footer class="w-full flex justify-between border-t border-grey-light mt-6 md:mt-2 pt-10">
+    <a class="btn-grey p-[18px]" href="#" onclick="event.preventDefault(); window.scrollTo({ top: 0, behavior: 'smooth' });">
+      <img src="@asset('images/arrow-up.svg')" alt="{{ __('Seta para cima') }}">
+    </a>
+
+    <a class="btn-secondary py-3 px-6 text-base" href="{{ get_home_url() }}">Acessar notícias</a>
+  </footer>
 </article>
